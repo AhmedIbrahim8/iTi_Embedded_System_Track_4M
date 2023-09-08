ab9f657724a6c04aa1af78834474f059576343bd
Ahmed Ibrahim <eng.ahmedibrahim21@gmail.com>
2023-09-08 18:51:18 +0300

    Update

--- 2_AVR_PROTEUS/TASKS/Real Time Clock/2_AVR/ATMEGA_32/MCAL/Timer2/Timer2_Cfg.h
+++ 2_AVR_PROTEUS/TASKS/Real Time Clock/2_AVR/ATMEGA_32/MCAL/Timer2/Timer2_Cfg.h
@@ -11,7 +11,7 @@
 #define MCAL_TIMER2_TIMER2_CFG_H_
 
 #define    TIMER2_CLOCK_SOURCE                   TIMER_2_EXTERNAL_CLOCK_SOURCE
-#define    TIMER2_MODE                           TIMER2_FAST_PWM
+#define    TIMER2_MODE                           TIMER2_CTC_MODE
 #define    TIMER2_OC2_NON_PWM   		         TOGGLE_OC2_ON_COMAPRE_MATCH
 #define	   TIMER2_OC2_FAST_PWM                   CLEAR_OC2_ON_COMPARE_MATCH_NON_INVERTING
 #define    TIMER2_OC2_PHASE_CORRECT_PWM	         TIMER2_NORMAL_PORT_PHASE_CORRECT_PWM
